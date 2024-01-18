cde56880aaa3be8dbe6dc81cb6ebad8b8feb1c48
Ivan Karpov <kivan8776w@gmail.com>
2024-01-18 15:22:27 +0200

    change neovim catppuccin flavour and add integrations

--- nvim/lua/plugins/catppuccin.lua
+++ nvim/lua/plugins/catppuccin.lua
@@ -3,6 +3,24 @@ return {
     name = "catppuccin",
     priority = 1000,
     config = function()
+        local catppuccin = require("catppuccin")
+
+        catppuccin.setup({
+            flavour = "frappe",
+            integrations = {
+                cmp = true,
+                gitsigns = true,
+                treesitter = true,
+                alpha = true,
+                mason = true,
+                mini = {
+                    enable = true,
+                    indentscope_color = "",
+                },
+                neotree = false,
+            }
+        })
+
         vim.cmd.colorscheme("catppuccin")
     end,
 }
