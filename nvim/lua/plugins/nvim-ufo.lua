8d9612b9e4e2c1d56bc0c7139361d4b870404045
Ivan Karpov <kivan8776w@gmail.com>
2024-01-20 10:35:12 +0200

    add code folding capabilities

---
+++ nvim/lua/plugins/nvim-ufo.lua
@@ -0,0 +1,65 @@
+return {
+    "kevinhwang91/nvim-ufo",
+    dependencies = {
+        "kevinhwang91/promise-async",
+    },
+    event = "BufRead",
+    opts = {
+        provider_selectro = function(bufnr, filetype, buftype)
+            return { "lsp", "indent" }
+        end,
+        fold_virt_text_handler = function(virtText, lnum, endLnum, width, truncate)
+            local newVirtText = {}
+            local suffix = (" 󰁂 %d "):format(endLnum - lnum)
+            local sufWidth = vim.fn.strdisplaywidth(suffix)
+            local targetWidth = width - sufWidth
+            local curWidth = 0
+            for _, chunk in ipairs(virtText) do
+                local chunkText = chunk[1]
+                local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+                if targetWidth > curWidth + chunkWidth then
+                    table.insert(newVirtText, chunk)
+                else
+                    chunkText = truncate(chunkText, targetWidth - curWidth)
+                    local hlGroup = chunk[2]
+                    table.insert(newVirtText, { chunkText, hlGroup })
+                    chunkWidth = vim.fn.strdisplaywidth(chunkText)
+                    -- str width returned from truncate() may less than 2nd argument, need padding
+                    if curWidth + chunkWidth < targetWidth then
+                        suffix = suffix .. (" "):rep(targetWidth - curWidth - chunkWidth)
+                    end
+                    break
+                end
+                curWidth = curWidth + chunkWidth
+            end
+            table.insert(newVirtText, { suffix, "MoreMsg" })
+            return newVirtText
+        end,
+    },
+    config = function(_, opts)
+        local ufo = require("ufo")
+
+        local capabilities = vim.lsp.protocol.make_client_capabilities()
+        capabilities.textDocument.foldingRange = {
+            dynamicRegistration = false,
+            lineFoldingOnly = true,
+        }
+        local language_servers = require("lspconfig").util.available_servers()
+        for _, ls in ipairs(language_servers) do
+            require("lspconfig")[ls].setup({
+                capabilities = capabilities,
+            })
+        end
+
+        ufo.setup(opts)
+
+        vim.keymap.set("n", "zR", ufo.openAllFolds, {})
+        vim.keymap.set("n", "zM", ufo.closeAllFolds, {})
+        vim.keymap.set("n", "zK", function()
+            local winid = ufo.peekFoldedLinesUnderCursor()
+            if not winid then
+                vim.lsp.buf.hover()
+            end
+        end)
+    end,
+}
--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -12,5 +12,8 @@ opt.shiftwidth = 4
 opt.number = true
 -- opt.relativenumber = true
 opt.list = true
-
+opt.foldcolumn = "1"
+opt.foldlevel = 99
+opt.foldlevelstart = 99
+opt.foldenable = true
 
