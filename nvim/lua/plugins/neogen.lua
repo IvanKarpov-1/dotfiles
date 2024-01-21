51ffa0213bd07689b5ee111ac6d850538b5d6dab
Ivan Karpov <kivan8776w@gmail.com>
2024-01-21 14:39:57 +0200

    neovim: add Neogen plugin

---
+++ nvim/lua/plugins/neogen.lua
@@ -0,0 +1,13 @@
+return {
+    "danymat/neogen",
+    dependencies = "nvim-treesitter/nvim-treesitter",
+    config = function()
+        local neogen = require("neogen")
+
+        neogen.setup({
+            snippet_engine = "luasnip",
+        })
+
+        vim.keymap.set("n", "<leader>nf", neogen.generate, {})
+    end,
+}
