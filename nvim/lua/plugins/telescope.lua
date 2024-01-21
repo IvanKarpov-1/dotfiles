d00cbd17a4b37b478df81a17bde041e2d9ac975f
Ivan Karpov <kivan8776w@gmail.com>
2024-01-21 14:28:24 +0200

    neovim: add telescope-undo.nvim plugin

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -5,6 +5,8 @@ return {
         dependencies = {
             "nvim-lua/plenary.nvim",
             "sharkdp/fd",
+            "nvim-telescope/telescope-ui-select.nvim",
+            "debugloop/telescope-undo.nvim",
         },
         config = function()
             local telescope = require("telescope")
@@ -14,19 +16,25 @@ return {
                     ["ui-select"] = {
                         require("telescope.themes").get_dropdown({}),
                     },
+                    ["undo"] = {
+                        side_by_side = true,
+                        layout_strategy = "vertical",
+                        layout_config = {
+                            preview_height = 0.8,
+                        },
+                    },
                 },
             })
 
             local builtin = require("telescope.builtin")
             vim.keymap.set("n", "<C-P>", builtin.find_files, {})
             vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-            vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+            vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+            vim.keymap.set("n", "<leader>u", "<cmd>Telescope undo<CR>", {})
 
             telescope.load_extension("ui-select")
             telescope.load_extension("noice")
+            telescope.load_extension("undo")
         end,
-    },
-    {
-        "nvim-telescope/telescope-ui-select.nvim",
     },
 }
