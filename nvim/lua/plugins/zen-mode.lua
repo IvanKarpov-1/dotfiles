35049691c26871ed129424385c8e409d11b72d3b
Ivan Karpov <kivan8776w@gmail.com>
2024-01-29 17:08:33 +0200

    neovim: add zen mode plugin

---
+++ nvim/lua/plugins/zen-mode.lua
@@ -0,0 +1,13 @@
+return {
+    "folke/zen-mode.nvim",
+    opts = {},
+    keys = {
+        {
+            mode = "n",
+            "<leader>zt",
+            ":ZenMode<CR>",
+            silent = true,
+            desc = "Toggle ZenMode",
+        },
+    },
+}
