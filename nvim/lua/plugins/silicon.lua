80ff6cec2f463c6bcd1ce71ab4081d2b2ddcc6c8
Ivan Karpov <kivan8776w@gmail.com>
2024-02-05 18:52:58 +0200

    neovim: change output directory for silicon

--- nvim/lua/plugins/silicon.lua
+++ nvim/lua/plugins/silicon.lua
@@ -4,13 +4,13 @@ return {
     cmd = "Silicon",
     config = function()
         local silicon = require("silicon")
-
         silicon.setup({
             theme = "Catppuccin-macchiato",
             background = "#fff0",
             font = "RobotoMono Nerd Font Mono=13",
-            -- pad_horiz = 0,
-            -- pad_vert = 0,
+            pad_horiz = 0,
+            pad_vert = 0,
+            no_window_controls = true,
             line_offset = function(args)
                 return args.line1
             end,
@@ -18,7 +18,7 @@ return {
             shadow_offset_x = 0,
             shadow_offset_y = 0,
             output = function()
-                return "~/Картинки/Знімки екрана/silicon/" .. os.date("!%Y-%m-%dT%H-%M-%S") .. "_code.png"
+                return "~/Картинки/silicon/" .. os.date("!%Y-%m-%dT%H-%M-%S") .. "_code.png"
             end,
             to_clipboard = true,
         })
